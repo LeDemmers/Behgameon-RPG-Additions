cbf74359b560c9a6e703abbf40556c44cdf6439d
AzureZhen <rcraft@cyberconnects2.com>
2019-12-07 18:22:29 -0500

    Fixed it requiring Mine and Slash

--- src/main/java/com/demmers/behgameon/BehgameonMod.java
+++ src/main/java/com/demmers/behgameon/BehgameonMod.java
@@ -1,5 +1,10 @@
 package com.demmers.behgameon;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.demmers.behgameon.init.ModConfig;
+import com.demmers.behgameon.init.Tab;
 import com.demmers.behgameon.proxy.CommonProxy;
 import com.demmers.behgameon.util.LootHandler;
 import com.demmers.behgameon.util.MineSlashHandler;
@@ -15,13 +20,6 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
-import com.demmers.behgameon.init.ModConfig;
-import com.demmers.behgameon.init.ModItems;
-import com.demmers.behgameon.init.Tab;
-
 @Mod(modid = BehgameonMod.MODID, name = BehgameonMod.NAME, version = BehgameonMod.VERSION, dependencies = BehgameonMod.DEPENDENCIES)
 public class BehgameonMod {
 
@@ -30,7 +28,7 @@ public class BehgameonMod {
 	// Mod name
 	public static final String NAME = "Behgameon";
 	// Verison number using SymVer MAJOR.MINOR.PATCH.
-	public static final String VERSION = "1.0.2";
+	public static final String VERSION = "1.0.3";
 	// So no one tries to run this on 1.7.10
 	public static final String ACCEPTED_VERSIONS = "(1.12.2)";
 	// Makes the mod require that is loaded after baubles and mine and slash

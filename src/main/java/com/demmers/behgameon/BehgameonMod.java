21b85d0514fb1443b92151534daa0449f2ee1f06
AzureZhen <rcraft@cyberconnects2.com>
2019-12-30 21:45:39 -0500

    v1.1.0 Curios reenabled for 1.15.1!

--- src/main/java/com/demmers/behgameon/BehgameonMod.java
+++ src/main/java/com/demmers/behgameon/BehgameonMod.java
@@ -8,7 +8,7 @@ import com.demmers.behgameon.util.LootHandler;
 import com.demmers.behgameon.util.MineSlashHandler;
 
 import net.minecraftforge.common.MinecraftForge;
-//import net.minecraftforge.fml.InterModComms;
+import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
@@ -17,8 +17,8 @@ import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.fml.loading.FMLPaths;
-//import top.theillusivec4.curios.api.CuriosAPI;
-//import top.theillusivec4.curios.api.imc.CurioIMCMessage;
+import top.theillusivec4.curios.api.CuriosAPI;
+import top.theillusivec4.curios.api.imc.CurioIMCMessage;
 
 @Mod(BehgameonMod.MODID)
 public class BehgameonMod {
@@ -46,11 +46,11 @@ public class BehgameonMod {
 	private void enqueueIMC(final InterModEnqueueEvent event) {
 		// If Curios is installed, then it will link the items to curios slots
 		if (ModList.get().isLoaded("curios")) {
-			//InterModComms.sendTo("curios", CuriosAPI.IMC.REGISTER_TYPE, () -> new CurioIMCMessage("charm"));
-			//InterModComms.sendTo("curios", CuriosAPI.IMC.REGISTER_TYPE, () -> new CurioIMCMessage("necklace"));
-			//InterModComms.sendTo("curios", CuriosAPI.IMC.REGISTER_TYPE, () -> new CurioIMCMessage("bracelet"));
-			//InterModComms.sendTo("curios", CuriosAPI.IMC.REGISTER_TYPE, () -> new CurioIMCMessage("belt"));
-			//InterModComms.sendTo("curios", CuriosAPI.IMC.REGISTER_TYPE, () -> new CurioIMCMessage("ring").setSize(2));
+			InterModComms.sendTo("curios", CuriosAPI.IMC.REGISTER_TYPE, () -> new CurioIMCMessage("charm"));
+			InterModComms.sendTo("curios", CuriosAPI.IMC.REGISTER_TYPE, () -> new CurioIMCMessage("necklace"));
+			InterModComms.sendTo("curios", CuriosAPI.IMC.REGISTER_TYPE, () -> new CurioIMCMessage("bracelet"));
+			InterModComms.sendTo("curios", CuriosAPI.IMC.REGISTER_TYPE, () -> new CurioIMCMessage("belt"));
+			InterModComms.sendTo("curios", CuriosAPI.IMC.REGISTER_TYPE, () -> new CurioIMCMessage("ring").setSize(2));
 		}
 		if (ModList.get().isLoaded("mmorpg") && Config.INSTANCE.USE_COMPATIBILITY_ON_ITEMS.get()) {
 			MinecraftForge.EVENT_BUS.register(new MineSlashHandler());

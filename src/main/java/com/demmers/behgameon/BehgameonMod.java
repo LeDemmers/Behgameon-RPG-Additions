00ada174954fe8fae16d3430115321df49a646ed
AzureZhen <rcraft@cyberconnects2.com>
2020-02-27 08:24:38 -0500

    v1.3.0

--- src/main/java/com/demmers/behgameon/BehgameonMod.java
+++ src/main/java/com/demmers/behgameon/BehgameonMod.java
@@ -7,14 +7,17 @@ import com.demmers.behgameon.config.Config;
 import com.demmers.behgameon.util.LootHandler;
 import com.demmers.behgameon.util.MineSlashHandler;
 
+import net.minecraft.data.DataGenerator;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.GatherDataEvent;
 import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.fml.loading.FMLPaths;
@@ -53,8 +56,18 @@ public class BehgameonMod {
 			InterModComms.sendTo("curios", CuriosAPI.IMC.REGISTER_TYPE, () -> new CurioIMCMessage("belt"));
 			InterModComms.sendTo("curios", CuriosAPI.IMC.REGISTER_TYPE, () -> new CurioIMCMessage("ring").setSize(2));
 		}
-		if (ModList.get().isLoaded("mmorpg") && Config.SERVER.USE_COMPATIBILITY_ON_ITEMS.get()) {
-			MinecraftForge.EVENT_BUS.register(new MineSlashHandler());
+	}
+
+	@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
+	public static class GatherDataSubscriber {
+		@SubscribeEvent
+		public static void gatherData(GatherDataEvent event) {
+			DataGenerator gen = event.getGenerator();
+			if (event.includeServer()) {
+				if (ModList.get().isLoaded("mmorpg") && Config.SERVER.USE_COMPATIBILITY_ON_ITEMS.get()) {
+					gen.addProvider(new MineSlashHandler().getDataPackCreator(gen));
+				}
+			}
 		}
 	}
 }
--- src/main/java/com/demmers/behgameon/util/MineSlashHandler.java
+++ src/main/java/com/demmers/behgameon/util/MineSlashHandler.java
@@ -1,59 +1,193 @@
 package com.demmers.behgameon.util;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.demmers.behgameon.BehgameonMod;
 import com.demmers.behgameon.config.Config;
-import com.robertx22.mine_and_slash.api.MineAndSlashAPI;
-import com.robertx22.mine_and_slash.config.compatible_items.ConfigItem;
+import com.robertx22.mine_and_slash.config.compatible_items.OldConfigItem;
+import com.robertx22.mine_and_slash.data_generation.compatible_items.CompatibleItem;
+import com.robertx22.mine_and_slash.data_generation.compatible_items.CompatibleItemProvider;
 import com.robertx22.mine_and_slash.database.gearitemslots.curios.Bracelet;
 import com.robertx22.mine_and_slash.database.gearitemslots.curios.Charm;
 import com.robertx22.mine_and_slash.database.gearitemslots.curios.Necklace;
 import com.robertx22.mine_and_slash.database.gearitemslots.curios.Ring;
 import com.robertx22.mine_and_slash.database.gearitemslots.offhand.Shield;
+import com.robertx22.mine_and_slash.onevent.data_gen.providers.SlashDataProvider;
+
+import net.minecraft.data.DataGenerator;
 
 public class MineSlashHandler {
 
-	public MineSlashHandler() {
+	public static String ID = "compatible_items";
 
-		// Runs accessory_1 to 3 and sets them as Bracelets in Mine and Slash.
-		for (int i = 1; i <= 3; i++)
-			MineAndSlashAPI.addCompatibleItem(BehgameonMod.MODID + ":accessory_" + i, new ConfigItem()
-					.setType(Bracelet.INSTANCE).setSalvagable(Config.SERVER.USE_MINESLASHLOOTSYSTEM.get()));
-		// Runs accessory_4 to 8 and sets them as Necklaces in Mine and Slash.
-		for (int i = 4; i <= 8; i++)
-			MineAndSlashAPI.addCompatibleItem(BehgameonMod.MODID + ":accessory_" + i, new ConfigItem()
-					.setType(Necklace.INSTANCE).setSalvagable(Config.SERVER.USE_MINESLASHLOOTSYSTEM.get()));
-		// Runs accessory_9 to 20 and sets them as Bracelets in Mine and Slash.
-		for (int i = 9; i <= 20; i++)
-			MineAndSlashAPI.addCompatibleItem(BehgameonMod.MODID + ":accessory_" + i, new ConfigItem()
-					.setType(Bracelet.INSTANCE).setSalvagable(Config.SERVER.USE_MINESLASHLOOTSYSTEM.get()));
-		// Runs accessory_22 to 32 and sets them as Necklaces in Mine and Slash.
-		for (int i = 21; i <= 32; i++)
-			MineAndSlashAPI.addCompatibleItem(BehgameonMod.MODID + ":accessory_" + i, new ConfigItem()
-					.setType(Necklace.INSTANCE).setSalvagable(Config.SERVER.USE_MINESLASHLOOTSYSTEM.get()));
-		// Runs accessory_33 to 48 and sets them as Charms in Mine and Slash.
-		for (int i = 33; i <= 44; i++)
-			MineAndSlashAPI.addCompatibleItem(BehgameonMod.MODID + ":accessory_" + i, new ConfigItem()
-					.setType(Charm.INSTANCE).setSalvagable(Config.SERVER.USE_MINESLASHLOOTSYSTEM.get()));
-		// Runs accessory_45 to 48 and sets them as Unique Charms in Mine and Slash.
-		for (int i = 45; i <= 48; i++)
-			MineAndSlashAPI.addCompatibleItem(BehgameonMod.MODID + ":accessory_" + i, new ConfigItem()
-					.setType(Charm.INSTANCE).setSalvagable(Config.SERVER.USE_MINESLASHLOOTSYSTEM.get()));
-		// Runs accessory_49 to 54 and sets them as Unique Necklace in Mine and Slash.
-		for (int i = 49; i <= 54; i++)
-			MineAndSlashAPI.addCompatibleItem(BehgameonMod.MODID + ":accessory_" + i,
-					new ConfigItem().setType(Necklace.INSTANCE).setAlwaysUnique()
-							.setSalvagable(Config.SERVER.USE_MINESLASHLOOTSYSTEM.get()));
-		// Runs accessory_55 to 56 and sets them as Unique Charms in Mine and Slash.
-		for (int i = 55; i <= 56; i++)
-			MineAndSlashAPI.addCompatibleItem(BehgameonMod.MODID + ":accessory_" + i, new ConfigItem()
-					.setType(Charm.INSTANCE).setSalvagable(Config.SERVER.USE_MINESLASHLOOTSYSTEM.get()));
-		// Runs accessory_49 to 54 and sets them as Rings in Mine and Slash.
-		for (int i = 1; i <= 106; i++)
-			MineAndSlashAPI.addCompatibleItem(BehgameonMod.MODID + ":ring_" + i,
-					new ConfigItem().setType(Ring.INSTANCE).setSalvagable(Config.SERVER.USE_MINESLASHLOOTSYSTEM.get()));
-		for (int i = 1; i <= 15; i++)
-			MineAndSlashAPI.addCompatibleItem(BehgameonMod.MODID + ":shield" + i,
-					new ConfigItem().setType(Shield.INSTANCE).setSalvagable(Config.SERVER.USE_MINESLASHLOOTSYSTEM.get()));
-		BehgameonMod.LOGGER.info("Registered Everything with Mine and Slash");
+	public MineSlashHandler() {
+	}
+
+	@SuppressWarnings("rawtypes")
+	public SlashDataProvider getDataPackCreator(DataGenerator gen) {
+		return new CompatibleItemProvider(gen, getList(), ID);
+	}
+
+	public static List<CompatibleItem> getList() {
+		List<CompatibleItem> items = new ArrayList<>();
+		try {
+			for (int type = 1; type <= 3; type++)
+				switch (type) {
+				case 1:
+					for (int braceletnumber = 1; braceletnumber <= 3; braceletnumber++) {
+						OldConfigItem braceletconfig = new OldConfigItem().setGenerationWeights(1000, 200, 0)
+								.setMaxRarity(5).setMinRarity(0).setSalvagable(true).setType(Bracelet.INSTANCE)
+								.setdropsAsLoot(Config.SERVER.USE_MINESLASHLOOTSYSTEM.get());
+
+						String id = BehgameonMod.MODID + ":accessory_" + braceletnumber;
+
+						CompatibleItem bracelet = braceletconfig.convertToNewFormat();
+						bracelet.guid = id;
+						bracelet.item_id = id;
+
+						items.add(bracelet);
+					}
+				case 2:
+					for (int necklacenumber = 4; necklacenumber <= 8; necklacenumber++) {
+						OldConfigItem necklaceconfig = new OldConfigItem().setGenerationWeights(1000, 200, 0)
+								.setMaxRarity(5).setMinRarity(0).setSalvagable(true).setType(Necklace.INSTANCE)
+								.setdropsAsLoot(Config.SERVER.USE_MINESLASHLOOTSYSTEM.get());
+
+						String id2 = BehgameonMod.MODID + ":accessory_" + necklacenumber;
+						;
+
+						CompatibleItem necklace = necklaceconfig.convertToNewFormat();
+						necklace.guid = id2;
+						necklace.item_id = id2;
+
+						items.add(necklace);
+					}
+				case 3:
+					for (int braceletnumber = 9; braceletnumber <= 20; braceletnumber++) {
+						OldConfigItem braceletconfig = new OldConfigItem().setGenerationWeights(1000, 200, 0)
+								.setMaxRarity(5).setMinRarity(0).setSalvagable(true).setType(Bracelet.INSTANCE)
+								.setdropsAsLoot(Config.SERVER.USE_MINESLASHLOOTSYSTEM.get());
+
+						String id3 = BehgameonMod.MODID + ":accessory_" + braceletnumber;
+						;
+
+						CompatibleItem bracelet = braceletconfig.convertToNewFormat();
+						bracelet.guid = id3;
+						bracelet.item_id = id3;
+
+						items.add(bracelet);
+					}
+				case 4:
+					for (int necklacenumber = 21; necklacenumber <= 32; necklacenumber++) {
+						OldConfigItem necklaceconfig = new OldConfigItem().setGenerationWeights(1000, 200, 0)
+								.setMaxRarity(5).setMinRarity(0).setSalvagable(true).setType(Necklace.INSTANCE)
+								.setdropsAsLoot(Config.SERVER.USE_MINESLASHLOOTSYSTEM.get());
+
+						String id4 = BehgameonMod.MODID + ":accessory_" + necklacenumber;
+						;
+
+						CompatibleItem necklace = necklaceconfig.convertToNewFormat();
+						necklace.guid = id4;
+						necklace.item_id = id4;
+
+						items.add(necklace);
+					}
+				case 5:
+					for (int charmnumber = 33; charmnumber <= 44; charmnumber++) {
+						OldConfigItem charmconfig = new OldConfigItem().setGenerationWeights(1000, 200, 0)
+								.setMaxRarity(5).setMinRarity(0).setSalvagable(true).setType(Charm.INSTANCE)
+								.setdropsAsLoot(Config.SERVER.USE_MINESLASHLOOTSYSTEM.get());
+
+						String id5 = BehgameonMod.MODID + ":accessory_" + charmnumber;
+						;
+
+						CompatibleItem charm = charmconfig.convertToNewFormat();
+						charm.guid = id5;
+						charm.item_id = id5;
+
+						items.add(charm);
+					}
+				case 6:
+					for (int charmnumber = 45; charmnumber <= 48; charmnumber++) {
+						OldConfigItem charmconfig = new OldConfigItem().setGenerationWeights(1000, 200, 0)
+								.setMaxRarity(5).setMinRarity(0).setSalvagable(true).setType(Charm.INSTANCE)
+								.setdropsAsLoot(Config.SERVER.USE_MINESLASHLOOTSYSTEM.get());
+
+						String id6 = BehgameonMod.MODID + ":accessory_" + charmnumber;
+						;
+
+						CompatibleItem charm = charmconfig.convertToNewFormat();
+						charm.guid = id6;
+						charm.item_id = id6;
+
+						items.add(charm);
+					}
+				case 7:
+					for (int necklacenumber = 49; necklacenumber <= 54; necklacenumber++) {
+						OldConfigItem necklaceconfig = new OldConfigItem().setGenerationWeights(1000, 200, 0)
+								.setMaxRarity(5).setMinRarity(0).setSalvagable(true).setType(Necklace.INSTANCE)
+								.setdropsAsLoot(Config.SERVER.USE_MINESLASHLOOTSYSTEM.get());
+
+						String id7 = BehgameonMod.MODID + ":accessory_" + necklacenumber;
+						;
+
+						CompatibleItem necklace = necklaceconfig.convertToNewFormat();
+						necklace.guid = id7;
+						necklace.item_id = id7;
+
+						items.add(necklace);
+					}
+				case 8:
+					for (int charmnumber = 55; charmnumber <= 56; charmnumber++) {
+						OldConfigItem charmconfig = new OldConfigItem().setGenerationWeights(1000, 200, 0)
+								.setMaxRarity(5).setMinRarity(0).setSalvagable(true).setType(Charm.INSTANCE)
+								.setdropsAsLoot(Config.SERVER.USE_MINESLASHLOOTSYSTEM.get());
+
+						String id8 = BehgameonMod.MODID + ":accessory_" + charmnumber;
+						;
+
+						CompatibleItem charm = charmconfig.convertToNewFormat();
+						charm.guid = id8;
+						charm.item_id = id8;
+
+						items.add(charm);
+					}
+				case 9:
+					for (int ringnumber = 1; ringnumber <= 106; ringnumber++) {
+						OldConfigItem ringconfig = new OldConfigItem().setGenerationWeights(1000, 200, 0)
+								.setMaxRarity(5).setMinRarity(0).setSalvagable(true).setType(Ring.INSTANCE)
+								.setdropsAsLoot(Config.SERVER.USE_MINESLASHLOOTSYSTEM.get());
+
+						String id9 = BehgameonMod.MODID + ":ring_" + ringnumber;
+						;
+
+						CompatibleItem ring = ringconfig.convertToNewFormat();
+						ring.guid = id9;
+						ring.item_id = id9;
+
+						items.add(ring);
+					}
+				case 10:
+					for (int shieldnumber = 1; shieldnumber <= 15; shieldnumber++) {
+						OldConfigItem shieldconfig = new OldConfigItem().setGenerationWeights(1000, 200, 0)
+								.setMaxRarity(5).setMinRarity(0).setSalvagable(true).setType(Shield.INSTANCE)
+								.setdropsAsLoot(Config.SERVER.USE_MINESLASHLOOTSYSTEM.get());
+
+						String id10 = BehgameonMod.MODID + ":shield" + shieldnumber;
+						;
+
+						CompatibleItem shield = shieldconfig.convertToNewFormat();
+						shield.guid = id10;
+						shield.item_id = id10;
+
+						items.add(shield);
+					}
+				}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return items;
 	}
 }

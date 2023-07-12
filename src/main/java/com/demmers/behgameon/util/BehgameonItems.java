e64ddc9156c9559aa8617b9bbe4532afdc95ad90
DEMMERS <demintent@gmail.com>
2023-07-11 23:32:45 -0500

    So thought this would be nice...
    Fixed item stacking. No reason for equipment to stack up to 64, everything is now set to a stack size of 1.

--- src/main/java/com/demmers/behgameon/util/BehgameonItems.java
+++ src/main/java/com/demmers/behgameon/util/BehgameonItems.java
@@ -22,40 +22,40 @@ public final class BehgameonItems {
 		// Creates the belts
 		for (int i = 1; i <= 3; i++)
 			event.getRegistry().registerAll(
-					accessory = new BaublesItem("accessory_" + i, BaubleType.BELT).setCreativeTab(BehgameonTab.ACCESSORIES));
+					accessory = new BaublesItem("accessory_" + i, BaubleType.BELT).setCreativeTab(BehgameonTab.ACCESSORIES).setMaxStackSize(1));
 		// Creates the Necklaces
 		for (int i = 4; i <= 8; i++)
 			event.getRegistry()
-					.registerAll(new BaublesItem("accessory_" + i, BaubleType.AMULET).setCreativeTab(BehgameonTab.ACCESSORIES));
+					.registerAll(new BaublesItem("accessory_" + i, BaubleType.AMULET).setCreativeTab(BehgameonTab.ACCESSORIES).setMaxStackSize(1));
 		// Creates the Bracelets
 		for (int i = 9; i <= 20; i++)
 			event.getRegistry()
-					.registerAll(new BaublesItem("accessory_" + i, BaubleType.BELT).setCreativeTab(BehgameonTab.ACCESSORIES));
+					.registerAll(new BaublesItem("accessory_" + i, BaubleType.BELT).setCreativeTab(BehgameonTab.ACCESSORIES).setMaxStackSize(1));
 		// Creates the Amulets/Necklaces
 		for (int i = 21; i <= 32; i++)
 			event.getRegistry()
-					.registerAll(new BaublesItem("accessory_" + i, BaubleType.AMULET).setCreativeTab(BehgameonTab.ACCESSORIES));
+					.registerAll(new BaublesItem("accessory_" + i, BaubleType.AMULET).setCreativeTab(BehgameonTab.ACCESSORIES).setMaxStackSize(1));
 		// Creates the Charms
 		for (int i = 33; i <= 44; i++)
 			event.getRegistry()
-					.registerAll(new BaublesItem("accessory_" + i, BaubleType.CHARM).setCreativeTab(BehgameonTab.ACCESSORIES));
+					.registerAll(new BaublesItem("accessory_" + i, BaubleType.CHARM).setCreativeTab(BehgameonTab.ACCESSORIES).setMaxStackSize(1));
 		// Creates the Unique Charms
 		for (int i = 45; i <= 48; i++)
 			event.getRegistry()
-					.registerAll(new BaublesItem("accessory_" + i, BaubleType.CHARM).setCreativeTab(BehgameonTab.ACCESSORIES));
+					.registerAll(new BaublesItem("accessory_" + i, BaubleType.CHARM).setCreativeTab(BehgameonTab.ACCESSORIES).setMaxStackSize(1));
 		// Creates the Unique Amulet
 		for (int i = 49; i <= 54; i++)
 			event.getRegistry()
-					.registerAll(new BaublesItem("accessory_" + i, BaubleType.AMULET).setCreativeTab(BehgameonTab.ACCESSORIES));
+					.registerAll(new BaublesItem("accessory_" + i, BaubleType.AMULET).setCreativeTab(BehgameonTab.ACCESSORIES).setMaxStackSize(1));
 		// Creates the Unique Charms
 		for (int i = 55; i <= 56; i++)
 			event.getRegistry()
-					.registerAll(new BaublesItem("accessory_" + i, BaubleType.CHARM).setCreativeTab(BehgameonTab.ACCESSORIES));
+					.registerAll(new BaublesItem("accessory_" + i, BaubleType.CHARM).setCreativeTab(BehgameonTab.ACCESSORIES).setMaxStackSize(1));
 		for (int i = 1; i <= 106; i++)
-			event.getRegistry().register(new BaublesItem("ring_" + i, BaubleType.RING).setCreativeTab(BehgameonTab.ACCESSORIES));
+			event.getRegistry().register(new BaublesItem("ring_" + i, BaubleType.RING).setCreativeTab(BehgameonTab.ACCESSORIES).setMaxStackSize(1));
 		for (int i = 1; i <= 15; i++)
 			event.getRegistry().register(shield = new ItemShield().setRegistryName("shield" + i)
-					.setTranslationKey("shield" + i).setCreativeTab(BehgameonTab.WEAPONS));
+					.setTranslationKey("shield" + i).setCreativeTab(BehgameonTab.WEAPONS).setMaxStackSize(1));
 		BehgameonMod.LOGGER.info("All Items registered.");
 	}
 
